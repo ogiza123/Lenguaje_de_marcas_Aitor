7ae75b824cf36ee5c84dd0a5e981067de385331d
unknown <Aitor Ogiza@UH-013-15.uni.lan>
2020-01-08 09:01:38 +0100

    s

---
+++ Ariketak_eval2/echo5.php
@@ -0,0 +1,19 @@
+<?php
+
+    echo $_GET["username"];
+    echo"<br>";
+if (isset($_GET["user"]))
+    echo $_GET["email"];
+    echo"<br>";
+    echo $_GET["psw"];
+    echo"<br>";
+if (isset($_GET["vehicle"]))
+    echo $_GET["vehicle"];
+    echo"<br>";    
+    echo $_GET["firstname"];
+    echo"<br>";
+    echo $_GET["lastname"];
+    echo"<br>";
+    echo $_GET["gender"];
+    echo"<br>";
+    echo $_GET["message"];
